f96d6f8a64d4d43d02afd599632f85c1f0efde5f
JAKE <paajake@gmail.com>
2024-05-30 11:31:45 +0000

    adds a testcontainers configured test

---
+++ src/test/java/dev/paajake/url_shortener/user/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package dev.paajake.url_shortener.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+	@Container
+	static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8");
+	@Autowired
+	UserRepository userRepository;
+
+	@Test
+	void dbConnectionEstablished() {
+		assertThat(mysqlContainer.isRunning()).isTrue();
+	}
+
+	@Test
+	void findUserByUsername() {
+	}
+}
