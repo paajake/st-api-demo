d1a26e45cf434ff6c7414ee8917470e734cfd054
JAKE <paajake@gmail.com>
2024-05-30 19:42:04 +0000

    fixes flyway test container integration issues

--- src/test/java/dev/paajake/url_shortener/user/UserRepositoryTest.java
+++ src/test/java/dev/paajake/url_shortener/user/UserRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -12,10 +15,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @DataJpaTest
+@Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 	@Container
-	static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8").withStartupTimeoutSeconds(180);
+	@ServiceConnection
+	static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8");
+
 	@Autowired
 	UserRepository userRepository;
 
@@ -24,7 +30,10 @@ class UserRepositoryTest {
 		assertThat(mysqlContainer.isRunning()).isTrue();
 	}
 
+	@Rollback
 	@Test
 	void findUserByUsername() {
 	}
+
+
 }
