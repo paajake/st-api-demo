603da49b7be4fdb642097f2c8320f58aea31e918
JAKE <paajake@gmail.com>
2024-05-30 19:51:36 +0000

    disables default test

--- src/test/java/dev/paajake/url_shortener/UrlShortenerApplicationTests.java
+++ src/test/java/dev/paajake/url_shortener/UrlShortenerApplicationTests.java
@@ -1,9 +1,11 @@
 package dev.paajake.url_shortener;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class UrlShortenerApplicationTests {
 
 	@Test
--- src/test/java/dev/paajake/url_shortener/user/UserRepositoryTest.java
+++ src/test/java/dev/paajake/url_shortener/user/UserRepositoryTest.java
@@ -35,5 +35,4 @@ class UserRepositoryTest {
 	void findUserByUsername() {
 	}
 
-
 }
